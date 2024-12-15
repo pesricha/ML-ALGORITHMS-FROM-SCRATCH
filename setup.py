30b58ec7f9d24d92afd9fc7fa72aa2dc6b9ac2bd
pesricha <naman_p@me.iitr.ac.in>
2024-12-15 21:50:29 +0530

    Updated setup.py two install dependencies

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="ml_algo_lib",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "scikit-learn",  # For loading datasets
+        "numpy",         # For numerical computations
+        "matplotlib"     # For data visualization
+    ],
 )
